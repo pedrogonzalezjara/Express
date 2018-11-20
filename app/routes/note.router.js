5f70dba7594f900c8edff216f87b3cfbffc24213
Pedro <pdro.gonzalez@gmail.com>
2018-11-19 22:23:08 -0300

    agregados archivos base del proyecti

---
+++ app/routes/note.router.js
@@ -0,0 +1,21 @@
+var express = require('express')
+var router = express.Router()
+
+const notes = require('../controllers/note.controller.js');
+
+// Create a new Note
+router.post('/notes', notes.create);
+
+// Retrieve all Notes
+router.get('/notes', notes.findAll);
+
+// Retrieve a single Note with noteId
+router.get('/notes/:noteId', notes.findOne);
+
+// Update a Note with noteId
+router.put('/notes/:noteId', notes.update);
+
+// Delete a Note with noteId
+router.delete('/notes/:noteId', notes.delete);
+
+module.exports = router
